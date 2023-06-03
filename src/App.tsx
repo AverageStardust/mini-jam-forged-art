9e8c68ee3f42229020b386205b6e45446a811d19
Martin Tale <me@martintale.com>
2023-06-03 21:52:47 +0100

    scores

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
 	return <>
 		<Show when={painting() == null}>
-			<h1>Art Forgery!</h1>
+			<h1>One Stroke Forger!</h1>
 
 			<p class="moto">See original, Forge it, Profit...</p>
 
@@ -21,6 +21,7 @@ function App() {
 					<img src="./level_1_thumbnail.png"></img>
 					<Show when={paintingIsComplete().has("sonOfMan")}>
 						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 11.386l1.17-1.206c1.951.522 5.313 1.731 8.33 3.597 3.175-4.177 9.582-9.398 13.456-11.777l1.044 1.073-14 18.927-10-10.614z" /></svg>
+						<strong class="score">${Math.floor((paintingIsComplete().get("sonOfMan") ?? 0) * 1000)}k</strong>
 					</Show>
 				</div>
 				<div classList={{
@@ -30,6 +31,7 @@ function App() {
 					<img src="./level_2_thumbnail.png"></img>
 					<Show when={paintingIsComplete().has("natureMorte")}>
 						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 11.386l1.17-1.206c1.951.522 5.313 1.731 8.33 3.597 3.175-4.177 9.582-9.398 13.456-11.777l1.044 1.073-14 18.927-10-10.614z" /></svg>
+						<strong class="score">${Math.floor((paintingIsComplete().get("natureMorte") ?? 0) * 1000)}k</strong>
 					</Show>
 				</div>
 				<div classList={{
@@ -39,6 +41,7 @@ function App() {
 					<img src="./level_3_thumbnail.png"></img>
 					<Show when={paintingIsComplete().has("theRoom")}>
 						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 11.386l1.17-1.206c1.951.522 5.313 1.731 8.33 3.597 3.175-4.177 9.582-9.398 13.456-11.777l1.044 1.073-14 18.927-10-10.614z" /></svg>
+						<strong class="score">${Math.floor((paintingIsComplete().get("theRoom") ?? 0) * 1000)}k</strong>
 					</Show>
 				</div>
 			</div>
